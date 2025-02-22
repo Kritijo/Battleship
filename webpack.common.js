3f16183d233765c919411871f8417da4f02beaf2
KRITI JOSHI <kritijoshi@KRITIs-MacBook-Pro.local>
2025-02-22 12:24:25 +0530

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: {
+        main: "./src/index.js",
+        game: "./src/game.js",
+        info: "./src/info.js",
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/pages/home.html",
+            filename: "index.html",
+            chunks: ["runtime", "vendors", "main"],
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/pages/game.html",
+            filename: "game.html",
+            chunks: ["runtime", "vendors", "game"],
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/pages/info.html",
+            filename: "info.html",
+            chunks: ["runtime", "vendors", "info"],
+        }),
+    ],
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "assets/images/[name][ext]",
+                },
+            },
+        ],
+    },
+};
